fd0ce7ca9ed24da7a2931e7354c9fa9229cd33ec
onejemee <84356486+onejemee@users.noreply.github.com>
2021-12-05 17:53:09 -0600

    Add files via upload

---
+++ beatsPM/src/main/java/com/example/beatsPM/BeatsPmApplication.java
@@ -0,0 +1,13 @@
+package com.example.beatsPM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatsPmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatsPmApplication.class, args);
+	}
+
+}
---
+++ beatsPM/src/test/java/com/example/beatsPM/BeatsPmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beatsPM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatsPmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
