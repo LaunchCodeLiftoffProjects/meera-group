ce37ff2c038517b8b6535ebe408b11b6c98dca5f
tim2197 <t.herron633@gmail.com>
2021-12-13 18:51:13 -0600

    started building Html form and angular code for post creation form

--- beatsPM/src/main/UI/src/app/createpost/createpost.component.ts
+++ beatsPM/src/main/UI/src/app/createpost/createpost.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-createpost',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./createpost.component.css']
 })
 export class CreatepostComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  
+  postCreation = {
+    postBody: '',
+    postTitle: '',
+    genre: ''
   }
 
+  createPostForm = new FormGroup({
+    postBody: new FormControl(''),
+    postTitle: new FormControl(''),
+    genres: new FormControl(),
+  });
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    };
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.createPostForm = this.fb.group({
+      postTitle: '',
+      postBody: '',
+      genres: '',
+      });
+    }
+
+    get f() {
+      return this.createPostForm.controls;
+    }
+
+  onSubmit(): void {
+    console.log(this.createPostForm);
+    
+  }
 }
