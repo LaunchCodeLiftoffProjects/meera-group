d05093a75c87398fd9f5f0f0cd28eeb25f0a3363
Layla <laylaesarhan@gmail.com>
2021-12-15 21:40:18 -0600

    post controller, model, and repository.

---
+++ beatsPM/src/main/java/com/example/beatsPM/Models/Data/postRepository.java
@@ -0,0 +1,10 @@
+import org.springframework.data.repository.CrudRepository;
+import beatsPM/src/main/java/com/example/beatsPM/Models/postModel.java;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface postRepository extends CrudRepository<postModel, Integer> {
+}
