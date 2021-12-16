efecaad162e1ef2d73774d5f803fd8c72bc3a41d
Ezugo Onejeme <eonejeme.cca@gmail.com>
2021-12-16 08:15:45 -0600

    connected sql database

--- beatsPM/src/main/java/com/example/beatsPM/Models/postModel.java
+++ beatsPM/src/main/java/com/example/beatsPM/Models/postModel.java
@@ -1,9 +1,21 @@
 package com.example.beatsPM.Models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class postModel {
+    @NotNull
     private String genre;
+    @NotNull
     private String postTitle;
+    @NotNull
     private String postBody;
+    @GeneratedValue
+    @Id
+    private int postId;
 
     public postModel() {
 
@@ -31,5 +43,9 @@ public class postModel {
 
     public void setPostBody(String postBody) {
         this.postBody = postBody;
+    }
+
+    public int getPostId() {
+        return postId;
     }
 }
