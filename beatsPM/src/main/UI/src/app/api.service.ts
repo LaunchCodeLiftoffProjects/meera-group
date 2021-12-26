4f5a61b25110e585d39047a7cd5a46477a9918ab
Tim Herron <t.herron633@gmail.com>
2021-12-26 11:52:54 -0600

    updated angular side of code for createPost connection

--- beatsPM/src/main/UI/src/app/api.service.ts
+++
@@ -1,8 +0,0 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { postCreationObject } from './createpost/postCreationObject';
- 
-@Injectable({providedIn:'root'})
-export class ApiService {
-}
