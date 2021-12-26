f141f9ede96b34a47dcc51f2f9d4351e41f880d0
Tim Herron <t.herron633@gmail.com>
2021-12-26 12:14:56 -0600

    adapted controller to handle postmodel class into SQL, changed springbootapp to exclude the security auto config to make sure it could accept the http requests without upsetting CORS everytime

--- beatsPM/src/main/java/com/example/beatsPM/Controllers/PostController.java
+++ beatsPM/src/main/java/com/example/beatsPM/Controllers/PostController.java
@@ -3,18 +3,24 @@ package com.example.beatsPM.Controllers;
 import com.example.beatsPM.Models.Data.PostRepository;
 import com.example.beatsPM.Models.PostModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:4200")
 public class PostController {
 
         @Autowired
         private PostRepository postRepository;
 
+        @CrossOrigin(origins = "http://localhost:4200/createpost/")
         @PostMapping("/add")
-        public void addPost(PostModel postModel) {
+        public ResponseEntity<Void> createPost(@RequestBody PostModel postModel) {
                 postRepository.save(postModel);
-        }
-
+                return new ResponseEntity<>(HttpStatus.CREATED);
+                }
         }
