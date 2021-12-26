c9dde75039eb8ef17a586c2c6ee91124816c78f8
Tim Herron <t.herron633@gmail.com>
2021-12-26 12:34:52 -0600

    created getmapping controller method to get all posts and put them into a list using spliterator

--- beatsPM/src/main/java/com/example/beatsPM/Controllers/PostController.java
+++ beatsPM/src/main/java/com/example/beatsPM/Controllers/PostController.java
@@ -5,22 +5,36 @@ import com.example.beatsPM.Models.PostModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.ResponseEntity.status;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 @Controller
-@CrossOrigin(origins = "http://localhost:4200")
 public class PostController {
 
         @Autowired
         private PostRepository postRepository;
 
-        @CrossOrigin(origins = "http://localhost:4200/createpost/")
         @PostMapping("/add")
         public ResponseEntity<Void> createPost(@RequestBody PostModel postModel) {
                 postRepository.save(postModel);
                 return new ResponseEntity<>(HttpStatus.CREATED);
                 }
+
+        @GetMapping("/forum")
+        public List<PostModel> getAllPosts() {
+                return StreamSupport.stream(postRepository.findAll().spliterator(), false)
+                        .collect(Collectors.toList());
         }
+        };
+
