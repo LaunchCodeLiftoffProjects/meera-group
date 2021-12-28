2f7566a8292c1f70c27ac0c68eeeeb02b642dd20
tim2197 <t.herron633@gmail.com>
2021-12-28 17:10:11 -0600

    updated apiconnection in springboot for crossorigin requests

--- beatsPM/src/main/java/com/example/beatsPM/Controllers/PostController.java
+++ beatsPM/src/main/java/com/example/beatsPM/Controllers/PostController.java
@@ -4,37 +4,36 @@ import com.example.beatsPM.Models.Data.PostRepository;
 import com.example.beatsPM.Models.PostModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import static java.util.stream.Collectors.toList;
-import static org.springframework.http.ResponseEntity.status;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+@CrossOrigin(origins = "*")
 @Controller
 public class PostController {
 
         @Autowired
         private PostRepository postRepository;
 
+        @CrossOrigin(origins = "http://localhost:4200/add")
         @PostMapping("/add")
         public ResponseEntity<Void> createPost(@RequestBody PostModel postModel) {
                 postRepository.save(postModel);
                 return new ResponseEntity<>(HttpStatus.CREATED);
                 }
 
+        @CrossOrigin(origins = "*")
         @GetMapping("/forum")
         public List<PostModel> getAllPosts() {
                 return StreamSupport.stream(postRepository.findAll().spliterator(), false)
                         .collect(Collectors.toList());
         }
-        };
+        }
 
