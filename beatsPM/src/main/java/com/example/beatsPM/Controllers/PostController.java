dc8374867860e6d3e3cc8c2a8df390f7831d286a
tim2197 <t.herron633@gmail.com>
2021-12-28 17:26:46 -0600

    fixed getmapping controller method, tested with angular to run the get request and localhost:4200 was able to get the returned json body

--- beatsPM/src/main/java/com/example/beatsPM/Controllers/PostController.java
+++ beatsPM/src/main/java/com/example/beatsPM/Controllers/PostController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,11 +26,10 @@ public class PostController {
                 return new ResponseEntity<>(HttpStatus.CREATED);
                 }
 
-        @CrossOrigin(origins = "*")
+        @CrossOrigin(origins = "http://localhost:4200/forum")
         @GetMapping("/forum")
-        public List<PostModel> getAllPosts() {
-                return StreamSupport.stream(postRepository.findAll().spliterator(), false)
-                        .collect(Collectors.toList());
+        public @ResponseBody Iterable<PostModel> getAllPosts() {
+                return postRepository.findAll();
         }
         }
 
