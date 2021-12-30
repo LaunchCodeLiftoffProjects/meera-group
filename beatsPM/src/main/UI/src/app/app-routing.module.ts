825c41a48555a4be73b08c1fa5400e3743c012b4
Fe1ky <76542408+Fe1ky@users.noreply.github.com>
2021-12-29 19:40:57 -0600

    created new component for Editing a post, edit button works and takes you an editPost page with all the post info filled in ready to edit.
    still need to make submit button work

--- beatsPM/src/main/UI/src/app/app-routing.module.ts
+++ beatsPM/src/main/UI/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CreatepostComponent } from './createpost/createpost.component'
+import { EditpostComponent } from './editpost/editpost.component'
 import { EdmComponent } from './forum/edm/edm.component';
 import { JazzComponent } from './forum/jazz/jazz.component';
 import { RockComponent } from './forum/rock/rock.component';
 import { RapComponent } from './forum/rap/rap.component';
 import { MiscComponent } from './forum/misc/misc.component';
 import { ForumComponent } from './forum/forum.component';
- 
+
 const routes: Routes = [
 
   {
@@ -27,6 +28,11 @@ const routes: Routes = [
     path: 'forum',
     pathMatch: 'full',
     component: ForumComponent,
+  },
+  {
+    path: 'editpost/:id',
+    pathMatch: 'prefix',
+    component: EditpostComponent
   },
 
 ];
