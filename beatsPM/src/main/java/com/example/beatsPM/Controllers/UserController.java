cc02926f8319790c9f8933fddd784eb36a3c34fe
Ezugo Onejeme <eonejeme.cca@gmail.com>
2022-01-06 18:45:32 -0600

    Added to user controller and model

--- beatsPM/src/main/java/com/example/beatsPM/Controllers/UserController.java
+++ beatsPM/src/main/java/com/example/beatsPM/Controllers/UserController.java
@@ -1,9 +1,34 @@
-package com.example.beatsPM.controllers;
+package com.example.beatsPM.Controllers;
 
 import com.example.beatsPM.Models.Data.UserRepository;
+import com.example.beatsPM.Models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+//Authentication
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        return user.get();
+    }
+
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getUserId());
+    }
 }
--- beatsPM/src/main/java/com/example/beatsPM/Models/User.java
+++ beatsPM/src/main/java/com/example/beatsPM/Models/User.java
@@ -1,5 +1,7 @@
 package com.example.beatsPM.Models;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -18,10 +20,50 @@ public class User {
     private String email;
     @NotNull
     @Size(min = 8)
-    private String password;
+    private String pwHash;
     @GeneratedValue
     @Id
     private int userId;
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public User (String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.pwHash = encoder.encode(password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPwHash() {
+        return pwHash;
+    }
+
+    public void setPwHash(String pwHash) {
+        this.pwHash = pwHash;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
 
     public User () {};
 }
